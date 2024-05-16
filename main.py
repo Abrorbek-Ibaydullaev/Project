e1635984f9886bd2ae71805e5751ce00fab4d42e
Abrorbek <abrorbekibaydullaev@gmail.com>
2024-05-16 02:28:34 +0200

    Started a new chapter

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-class Bank():
-  pass
-class Currency():
-  pass
+class Currency:
+  def __init__(self,value,symbol=None,abbreviation=None):
+    self.value = value
+    self.symbol = symbol
+    self.abbreviation = abbreviation
+  def __repr__(self) -> str:
+    return "This is just for test and returns Main Class"
+class Convertor(Currency):
+  def Convert():
+    return "Converting is on the process"
